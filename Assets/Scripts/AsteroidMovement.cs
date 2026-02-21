bb252cc88fa8a508248063d728ecb26e689f4f10
Andrea <andreatrevi91@gmail.com>
2026-02-21 23:19:03 +0100

    Asteroids move like they have an orbit, cool!!!

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
+/// <summary>
+/// OLD version: la rotazione degli asteroidi funziona in maniera strana, iniziano a girare in cerchio dal basso verso l'alto generando una casualità interessante, come se avessero un ORBITA!!!
+/// </summary>
+//public class AsteroidMovement : MonoBehaviour
+//{
+//    private float fallSpeed = 25f;
+//    private float rotationSpeed;
+//    private float destroyY;
+
+//    void Start()
+//    {
+//        // Rotazione casuale
+//        rotationSpeed = Random.Range(-50f, 50f);
+//        Debug.Log("rotationSpeed: " + rotationSpeed);
+
+//        // Calcola bordo inferiore camera
+//        float camDistance = transform.position.z - Camera.main.transform.position.z;
+//        Debug.Log($"camDistance: {camDistance}");
+
+//        Vector2 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+//        destroyY = bottomLeft.y - 2f;
+//    }
+
+//    void Update()
+//    {
+//        // Movimento verso il basso
+//        transform.Translate(0, -fallSpeed * Time.deltaTime, 0);
+
+//        // Rotazione
+//        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+
+//        // Distruzione se esce sotto
+//        if (transform.position.y < destroyY)
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
+//}
+
 public class AsteroidMovement : MonoBehaviour
 {
-    private float fallSpeed = 25f;
+    public float fallSpeed = 25f;
     private float rotationSpeed;
     private float destroyY;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        // Rotazione casuale
-        rotationSpeed = Random.Range(-50f, 50f);
-        Debug.Log("rotationSpeed: " + rotationSpeed);
+        rb = GetComponent<Rigidbody2D>();
+
+        // Rotazione casuale (in gradi al secondo)
+        rotationSpeed = Random.Range(-180f, 180f);
 
         // Calcola bordo inferiore camera
         float camDistance = transform.position.z - Camera.main.transform.position.z;
@@ -25,8 +66,8 @@ public class AsteroidMovement : MonoBehaviour
         // Movimento verso il basso
         transform.Translate(0, -fallSpeed * Time.deltaTime, 0);
 
-        // Rotazione
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        // Rotazione tramite Rigidbody
+        rb.angularVelocity = rotationSpeed;
 
         // Distruzione se esce sotto
         if (transform.position.y < destroyY)
